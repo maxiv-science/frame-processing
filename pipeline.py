3903057d056c34883e7fd2eae3d3420a308b88fb
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2020-12-15 12:36:12 +0100

    Added the downsampling option, removed the q axes for now

--- pipeline.py
+++ pipeline.py
@@ -4,10 +4,27 @@ import json
 import time
 import h5py
 import numpy as np
+from numpy.lib.stride_tricks import as_strided
 from multiprocessing import Process
 from azint import AzimuthalIntegrator
 from bitshuffle import decompress_lz4
 import fabio
+
+INTERNAL_PORT = 5550
+
+class Downsampler(object):
+    """
+    Uses stride_tricks to quickly bin pixels in n-by-n groups. Faster
+    than a plain c-loop for n>8 or so, only a little slower for n<8.
+    """
+    def __init__(self, n):
+        self.n = n
+    def downsample(self, img):
+        n = self.n
+        strided = as_strided(img,
+            shape=(img.shape[0]//n, img.shape[1]//n, n, n),
+            strides=((img.strides[0]*n, img.strides[1]*n)+img.strides))
+        return strided.sum(axis=-1).sum(axis=-1)
 
 class Worker(Process):
     """
@@ -25,28 +42,30 @@ class Worker(Process):
         if do_integration:
             ai = AzimuthalIntegrator(*integration_params, create=False)
             self.process = lambda img: ai.integrate(img)
+        elif do_downsampling:
+            ds = Downsampler(*downsampling_params)
+            self.process = lambda img: ds.downsample(img)
         print('Constucting worker %u out of %u'%(worker_id, nworkers))
 
 class ZmqWorker(Worker):
-    def __init__(self, *args, pullhost='tcp://p-daq-cn-2', pullport=20001, pushport=5550, **kwargs):
+    def __init__(self, *args, pullhost='tcp://p-daq-cn-2', pullport=20001, **kwargs):
         super().__init__(*args, **kwargs)
         self.pullhost = pullhost
         self.pullport = pullport
-        self.pushport = pushport
 
     def run(self):
         context = zmq.Context()
         pull_sock = context.socket(zmq.PULL)
         pull_sock.connect('%s:%u'%(self.pullhost, self.pullport))
         push_sock = context.socket(zmq.PUSH)
-        push_sock.connect('tcp://localhost:%u'%self.pushport)
+        push_sock.connect('tcp://localhost:%u'%INTERNAL_PORT)
         while True:
             parts = pull_sock.recv_multipart(copy=False)
             header = json.loads(parts[0].bytes)
             if header['htype'] == 'image':
                 img = decompress_lz4(parts[1].buffer, header['shape'], np.dtype(header['type']))
                 res = self.process(img)
-                header['type'] = 'float32'
+                header['type'] = str(res.dtype)
                 header['shape'] = res.shape
                 header['compression'] = 'none'
                 push_sock.send_json(header, flags=zmq.SNDMORE)
@@ -55,15 +74,14 @@ class ZmqWorker(Worker):
                 push_sock.send_json(header)
 
 class Hdf5Worker(Worker):
-    def __init__(self, *args, filename=None, pushport=5550, **kwargs):
+    def __init__(self, *args, filename=None, **kwargs):
         super().__init__(*args, **kwargs)
-        self.pushport = pushport
         self.filename = filename
 
     def run(self):
         context = zmq.Context()
         push_sock = context.socket(zmq.PUSH)
-        push_sock.connect('tcp://localhost:%u'%self.pushport)
+        push_sock.connect('tcp://localhost:%u'%INTERNAL_PORT)
         fh = h5py.File(self.filename, 'r')
         dset = fh['/entry/measurement/Eiger/data']
         nimages = len(dset)
@@ -76,7 +94,7 @@ class Hdf5Worker(Worker):
             img = dset[i]
             res = self.process(img)
             header = {'msg_number': i,
-                      'type': 'float32',
+                      'type': str(img.dtype),
                       'shape': res.shape,
                       'compression': 'none',
                       'htype': 'image',}
@@ -86,7 +104,7 @@ class Hdf5Worker(Worker):
                 closing = {'htype': 'series_end',
                            'msg_number': nimages,}
                 push_sock.send_json(closing)
-            
+
 def ordered_recv(sock):
     cache = {}
     next_msg_number = 0
@@ -111,20 +129,23 @@ def collector(radial_bins, phi_bins, disposable=False):
     base_folder = '/data/staff/nanomax/commissioning_2020-2/20201214_integ/process/radial_integration/'
     context = zmq.Context()
     pull_sock = context.socket(zmq.PULL)
-    pull_sock.bind('tcp://*:5550')
+    pull_sock.bind('tcp://*:%u'%INTERNAL_PORT)
     fh = None
-    dset = None
     for index, parts in ordered_recv(pull_sock):
         header = json.loads(parts[0])
         print(index, header)
         htype = header['htype']
         if htype == 'image':
+            print('******', header['type'])
             res = np.frombuffer(parts[1], header['type']).reshape(header['shape'])
+            #dsetname = header['dsetname']
+            dsetname = 'reduced'
             if fh:
+                dset = fh.get(dsetname)
                 if not dset:
-                    fh.create_dataset('q', data=0.5*(radial_bins[:-1] + radial_bins[1:]))
-                    fh.create_dataset('phi', data=0.5*(phi_bins[:-1] + phi_bins[1:]))
-                    dset = fh.create_dataset('cake', dtype=np.float32, 
+#                    fh.create_dataset('q', data=0.5*(radial_bins[:-1] + radial_bins[1:]))
+#                    fh.create_dataset('phi', data=0.5*(phi_bins[:-1] + phi_bins[1:]))
+                    dset = fh.create_dataset(dsetname, dtype=header['type'],
                                                shape=(0, *res.shape), 
                                                maxshape=(None, *res.shape),
                                                chunks=(1, *res.shape))
@@ -143,7 +164,6 @@ def collector(radial_bins, phi_bins, disposable=False):
                 fh = h5py.File(output_file, 'w-')
             else:
                 fh = None
-            dset = None
                 
         elif htype == 'series_end':
             print('end')
@@ -166,12 +186,13 @@ if __name__ == '__main__':
     procs = []
     for i in range(nworkers):
         p = ZmqWorker(i, nworkers, do_integration=True, integration_params=integ_pars)
+#        p = ZmqWorker(i, nworkers, do_downsampling=True, downsampling_params=[10,])
 #        p = Hdf5Worker(i, nworkers, do_integration=True, integration_params=integ_pars,
 #            filename='/data/staff/nanomax/commissioning_2020-2/20201214_integ/raw/sample/scan_000022_eiger.hdf5')
         p.start()
         procs.append(p)
 
-    collector(radial_bins, phi_bins)#, disposable=True)
+    collector(radial_bins, phi_bins)#, disposable=True) # set this for offline hdf5 processing
         
     for i in range(nworkers):
         procs[i].join()
